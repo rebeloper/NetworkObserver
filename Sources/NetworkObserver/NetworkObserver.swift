af8fc98a0e1f4bf9d7e2eca861b981d7356313d8
Alex Nagy <rebeloperalpar@gmail.com>
2024-10-24 11:02:02 +0300

    initial commit

--- Sources/NetworkObserver/NetworkObserver.swift
+++ Sources/NetworkObserver/NetworkObserver.swift
@@ -1,2 +1,34 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  NetworkObserver.swift
+//  
+//
+//  Created by Alex Nagy on 24.10.2024.
+//
+
+import SwiftUI
+import Network
+
+@Observable
+public class NetworkObserver: @unchecked Sendable {
+    private let networkMonitor = NWPathMonitor()
+    private var workerQueue = DispatchQueue(label: "NetworkObserver")
+    public var isConnected = false
+    
+    public init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+}
+
+public extension EnvironmentValues {
+    @Entry var network = NetworkObserver()
+}
+
+public extension View {
+    func listenToNetworkChanges() -> some View {
+        environment(\.network, .init())
+    }
+}
+
